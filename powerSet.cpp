86754daf9eb162d677e31fdb86bf28a4be6d1e1d
U-CITRITE\saravanand <saravanand@SJCLSARAVANA01.citrite.net>
2016-07-09 12:02:30 -0700

    Completed powerset and updated Puzzle Strategies

--- powerSet.cpp
+++ powerSet.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include <math.h>
 using namespace std;
 
 // Recursive solution
@@ -19,6 +20,8 @@ void recursivePowerSetUsingString(string number, string sofar) {
 	
 }
 
+// Recursive solution
+// Using vector as input. O(2^N)
 void recursivePowerSet(vector<int> &set, string sofar, int pos) {
 	if (pos >= set.size())
 		cout << "{" << sofar << "}" << endl;
@@ -28,9 +31,9 @@ void recursivePowerSet(vector<int> &set, string sofar, int pos) {
 		stringstream elm;
 		elm<<set[pos];
 		if (sofar.empty())
-			recursivePowerSet(set, sofar+elm.str(), pos + 1);
+			recursivePowerSet(set, sofar + elm.str(), pos + 1);
 		else
-			recursivePowerSet(set, sofar+","+elm.str(), pos + 1);
+			recursivePowerSet(set, sofar + "," + elm.str(), pos + 1);
 		recursivePowerSet(set, sofar, pos + 1);
 	}
 }
@@ -46,15 +49,38 @@ void printVector(vector<int> &set) {
 		cout << set[i] << endl;
 }
 
-// Iterative solution
+// Iterative solution uses set as input
+void iterativePowerSet(vector<int> &set) {
+	unsigned int powerSetSize = pow(2, set.size());
+	int maxBits = floor(log2(set.size()))+1;
+
+	for (unsigned int i = 0; i < powerSetSize; i++) {
+		cout << "{" ;
+		unsigned int bitSet = 1;
+		bool comma = false;
+		for (unsigned int j = 0; j <= maxBits; j++) {
+			if (i & (bitSet << j)) {
+				if (comma)
+					cout << "," << set[j];
+				else {
+					cout << set[j];
+					comma = true;
+				}
+			}
+		}
+		cout << "}" << endl;
+	}
+	
+}
 
 int main() {
-	cout << endl << "Using Strings as input :" << endl;
+	cout << endl << "Recursive powerset using Strings as input :" << endl;
 	recursivePowerSetUsingString("1234", "");
 	vector<int> set;
 	fillVector(set, 4);
-	cout << endl << "Using set as input : " << endl;
+	cout << endl << "Recursive powerset using set as input : " << endl;
 	recursivePowerSet(set,"", 0);
-//	iterativePowerSet(4);
+	cout << endl << "Iterative powerset using set as input : " << endl;
+	iterativePowerSet(set);
 	return 0;
 }
