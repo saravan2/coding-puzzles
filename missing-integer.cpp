7c7fdea4c133cba3dfb9a714020bc4b094e932ae
saravan2 <saravanand@outlook.com>
2017-11-20 19:11:06 -0800

    Missing integer

--- missing-integer.cpp
+++ missing-integer.cpp
@@ -1,13 +1,7 @@
-/* Header files */
 #include<iostream>
 #include<math.h>
-
-/* Globals */
-const int n = 10;
-const int m = floor(log2(n))+1;
-
-/* Integer sequence is stored in binary form with access to every bit
-   taking const time */
+const int n=10;
+const int m= floor(log2(n))+1;
 int A[m][n] = {
 	{0,0,1,0,0,0,0,1,1,0},
 	{0,0,0,1,1,1,0,0,0,0},
@@ -15,32 +9,30 @@ int A[m][n] = {
 	{1,1,0,1,0,1,0,1,0,0}
 	
 };
-
-/* O(n) algorithm to find the missing integer in this sequence */ 
 int main() {
-	int i, j, t, n0, n1, ntemp = n;
+	int i,j,t, n0, n1, ntemp=n;
 	int B[n], res[m];
 
-	/*  Initializing res, B arrays */
-	for (i = 0; i < m; i++)
-		res[i] = -1;
+	for (i=0; i<m; i++)
+		res[i]=-1;
 
-	for (i = 0; i < n; i++)
+	for (i=0; i<n; i++)
 		B[i]=i;
 
-	for (i = (m-1); i >= 0; i--) {
+	for (i=(m-1); i>=0; i--)
+	{
 		n0 = n1 = 0;
-
-		for (j = 0; j < ntemp; j++) {
+		for (j=0; j<ntemp; j++)
+		{
 			if (A[i][B[j]] == 0)
 				n0++;
 			else
 				n1++;
 		}
 
-		if ((n0 - n1 == 2) || (n0 - n1 == 1)) 
+		if (n0 - n1 == 2 || n0 - n1 == 1) 
 			res[i] = 1;
-		else if ((n0 - n1 == 0) || (n0 - n1 == -1)) 
+		else if (n0 - n1 == 0 || n0 - n1 == -1) 
 			res[i] = 0;
 		else
 			std::cout<<"Warning"<<std::endl;
@@ -60,7 +52,7 @@ int main() {
 			ntemp = floor(ntemp/2.0);
 	}
 
-	for (i = 0; i < m; i++)
+	for (i=0; i<m; i++)
 		std::cout << res[i];
 	std::cout<<std::endl;
 		
