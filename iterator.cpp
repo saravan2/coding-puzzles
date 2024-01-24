185ba5a682be850b9403c644aab57b03e24ef3d7
Saravanan D <saravanand@outlook.com>
2024-01-24 13:37:52 -0800

    Update iterator.cpp
    * Prevents copy of vectors during initialization
    * Album object initialization takes O(n) to fill up the indices of favorite and non-favorite indices
    * Randomized return of favorite and non-favorite photo iterators
    * getNextPhoto() always returns with constant time O(1) complexity

--- iterator.cpp
+++ iterator.cpp
@@ -12,44 +12,65 @@ public:
 
 class Album {
 private:
-    std::vector<Photo> photos;
+    //std::vector<Photo> photos;
+    const std::vector<Photo> &photos;
+    std::vector<size_t> favoritePhotoIndices;
     std::vector<size_t> nonFavoritePhotoIndices; 
     size_t currentNonFavoriteIndex; 
-    bool favoritePhotosRemaining;
-    std::vector<Photo>::const_iterator favoritePhotoIterator;
+    size_t currentFavoriteIndex;
+    //bool favoritePhotosRemaining;
+    //std::vector<Photo>::const_iterator favoritePhotoIterator;
 
 public:
     std::vector<Photo>::const_iterator sentinelIterator;
-    Album(const std::vector<Photo>& photos) : photos(photos), favoritePhotosRemaining(true) {
+    //Album(const std::vector<Photo>& photos) : photos(photos), favoritePhotosRemaining(true) {
+    Album(const std::vector<Photo>& photos) : photos(photos) {
         for (size_t i = 0; i < photos.size(); ++i) {
             if (!photos[i].favorite) {
                 nonFavoritePhotoIndices.push_back(i);
+            } else {
+                favoritePhotoIndices.push_back(i);
             }
         }
         std::random_device rd;
         std::mt19937 g(rd());
+        std::shuffle(favoritePhotoIndices.begin(), favoritePhotoIndices.end(), g);
         std::shuffle(nonFavoritePhotoIndices.begin(), nonFavoritePhotoIndices.end(), g);
+        currentFavoriteIndex = 0;
         currentNonFavoriteIndex = 0; 
-        favoritePhotoIterator = photos.begin();
+        //favoritePhotoIterator = photos.begin();
         sentinelIterator = photos.end();
     }
 
     // Function to get the next photo iterator, favoring favorites
-    std::vector<Photo>::const_iterator getNextPhoto() {
-        if (favoritePhotosRemaining) {
+    //std::vector<Photo>::const_iterator getNextPhoto() {
+        //if (favoritePhotosRemaining) {
             // Look for the next favorite photo starting from the last favorite photo
-            while(favoritePhotoIterator != sentinelIterator) {
-                if (favoritePhotoIterator->favorite) {
-                    favoritePhotoIterator++;
-                    return favoritePhotoIterator - 1;
-                } else {
-                    favoritePhotoIterator++;
-                }
-            }
-            favoritePhotosRemaining = false; 
-            favoritePhotoIterator = photos.begin();
+            //while(favoritePhotoIterator != sentinelIterator) {
+                //if (favoritePhotoIterator->favorite) {
+                    //favoritePhotoIterator++;
+                    //return favoritePhotoIterator - 1;
+                //} else {
+                    //favoritePhotoIterator++;
+                //}
+            //}
+            //favoritePhotosRemaining = false; 
+            //favoritePhotoIterator = photos.begin();
+        //}
+        //if (currentNonFavoriteIndex < nonFavoritePhotoIndices.size()) {
+            //size_t index = nonFavoritePhotoIndices[currentNonFavoriteIndex];
+            //++currentNonFavoriteIndex;
+            //return photos.begin() + index;
+        //}
+        //return sentinelIterator;
+    //}
+    
+    std::vector<Photo>::const_iterator getNextPhoto() {
+        if (currentFavoriteIndex < favoritePhotoIndices.size()) {
+            size_t index = favoritePhotoIndices[currentFavoriteIndex];
+            ++currentFavoriteIndex;
+            return photos.begin() + index;
         }
-
         if (currentNonFavoriteIndex < nonFavoritePhotoIndices.size()) {
             size_t index = nonFavoritePhotoIndices[currentNonFavoriteIndex];
             ++currentNonFavoriteIndex;
@@ -70,16 +91,24 @@ int main() {
   
     std::vector<Photo>::const_iterator photoIterator;
 
-    while (true) {
-        photoIterator = myAlbum.getNextPhoto();
-        if (photoIterator == myAlbum.sentinelIterator) {
-            break;
+    //while (true) {
+        //photoIterator = myAlbum.getNextPhoto();
+        //if (photoIterator == myAlbum.sentinelIterator) {
+        //    break;
+        //} else {
+            //if (photoIterator->favorite) {
+                //std::cout << "Favorite photo" << std::endl;
+            //} else {
+                //std::cout << "Non favorite photo" << std::endl;
+            //}
+        //}
+    //}
+    
+    while((photoIterator = myAlbum.getNextPhoto()) != myAlbum.sentinelIterator) {
+        if (photoIterator->favorite) {
+            std::cout << "Favorite photo" << std::endl;
         } else {
-            if (photoIterator->favorite) {
-                std::cout << "Favorite photo" << std::endl;
-            } else {
-                std::cout << "Non favorite photo" << std::endl;
-            }
+            std::cout << "Non favorite photo" << std::endl;
         }
     }
     return 0;
