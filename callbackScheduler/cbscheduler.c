d219f1cd01c26490000c7daceb7eb782594acffb
Saravanan D. <saravananinca@gmail.com>
2024-04-29 12:55:30 -0700

    Error handling changes

--- callbackScheduler/cbscheduler.c
+++ callbackScheduler/cbscheduler.c
@@ -6,6 +6,9 @@
 #include <timer.h>
 #include "cbscheduler.h"
 
+#define TQ_OK 0
+#define TQ_ERR -1
+
 // Structs
 typedef struct {
     void (*callback)();
@@ -17,6 +20,7 @@ typedef struct {
     unsigned int size;
     unsigned int capacity;
     timer_t tq_timerid;
+    uint64_t tq_timer_at;
     pthread_mutex_t tq_mutex;
     pthread_cond_t tq_cond;
     unsigned long long tq_mutex_head;
@@ -32,11 +36,12 @@ static uint64_t get_current_time_in_us();
 static void tq_fair_mutex_lock();
 static void tq_fair_mutex_unlock();
 static void tq_fair_mutex_unlock();
-static void tq_set_timer();
-static void tq_resize();
-static void tq_bubble_up();
-static void tq_bubble_down();
-static void tq_push(Task T);
+static int tq_set_timer();
+static int tq_resize();
+static void tq_bubble_up(unsigned int index);
+static void tq_bubble_down(unsigned int index);
+static void tq_remove(unsigned int index);
+static int tq_push(Task T)
 static Task tq_pop();
 static Task tq_front();
 static void timer_thread(void * arg);
@@ -66,39 +71,48 @@ static void tq_fair_mutex_unlock() {
     }
 
 }
-static void tq_set_timer() {
+static int tq_set_timer() {
+    int status = TQ_ERR;
     if (tq && tq->size && !tq_shutdown) {
         Task t = tq->tasks[0];
-        uint64_t current_time_us = get_current_time_in_us();
-        uint64_t delay_us = (t.execute_at > current_time_us)?(t.execute_at - current_time_us):1;
-        struct itimerspec its;
-        its.it_value.tv_sec = 0;
-        its.it_value.tv_nsec = 1000ULL*delay_us;
-        its.it_interval.tv_sec = 0;
-
-        if (timer_settime(&tq->tq_timerid, 0, &its, NULL) == -1) {
-            perror("Failed to set timer");
-            exit(-1);
+        if (t.execute_at != tq->tq_timer_at) {
+            uint64_t current_time_us = get_current_time_in_us();
+            uint64_t delay_us = (t.execute_at > current_time_us)?(t.execute_at - current_time_us):1;
+            struct itimerspec its;
+            its.it_interval.tv_sec = 0;
+            its.it_interval.tv_nsec = 0;
+            its.it_value.tv_sec = delay_us / 1000000ULL;
+            its.it_value.tv_nsec = (delay_us % 1000000ULL) * 1000ULL;
+            
+            if (timer_settime(&tq->tq_timerid, 0, &its, NULL)) {
+                perror("tq_set_timer() - Failed to set timer");
+            } else {
+                tq->tq_timer_at = t.execute_at;
+                status = TQ_OK;
+            }
         }
     }
+    return status;
 }
 
-static void tq_resize() {
+static int tq_resize() {
+    int status = TQ_ERR;
     if (tq) {
         unsigned int nc = TQ_RESIZE_FACTOR * tq->capacity;
         Task *nq = (Task*)realloc(tq->tasks, nc * sizeof(Task));
-        if (nq == tq->tasks) {
-            perror("Resizing tq failed");
-            exit(-1);
+        if (!nq) {
+            perror("tq_resize() - Resizing failed");
+            return status;
         }
         tq->tasks = nq;
         tq->capacity = nc;
+        status = TQ_OK;
     }
+    return status;
 }
 
-static void tq_bubble_up() {
+static void tq_bubble_up(unsigned int index) {
     if (tq) {
-        unsigned int index = tq->size;
         while(index > 0) {
             unsigned int parent = (index - 1) / 2;
             if (tq->tasks[index].execute_at < tq->tasks[parent].execute_at) {
@@ -113,10 +127,9 @@ static void tq_bubble_up() {
     }
 }
 
-static void tq_bubble_down() {
+static void tq_bubble_down(unsigned int index) {
     if (tq) {
         unsigned int n = tq->size;
-        unsigned int index = 0;
         while (index < n) {
             unsigned int left = (2 * index) + 1;
             unsigned int right = (2 * index) + 2;
@@ -127,7 +140,7 @@ static void tq_bubble_down() {
             }
 
             if (right < n && tq->tasks[right].execute_at < tq->tasks[smallest].execute_at) {
-                smallest = right;
+                smallest = right;Can we implement a hashing function
             }
 
             if (smallest != index) {
@@ -142,17 +155,42 @@ static void tq_bubble_down() {
     }
 }
 
-static void tq_push(Task T) {
+static void tq_remove(unsigned int index) {
+    if (tq && tq->size \
+        && index >= 0 \
+        && index < tq->size) {
+        tq->tasks[index] = tq->tasks[--tq->size];
+        if (index) {
+            // Compare with parent to know whether bubble up or down
+            int parent = (index - 1) / 2;
+            if (tq->tasks[index].execute_at < tq->tasks[parent].execute_at) {
+                tq_bubble_up(index);
+            } else {
+                tq_bubble_down(index);
+            }
+        } else {
+            tq_bubble_down(index);
+        }
+    }
+}
+
+static int tq_push(Task T) {
+    int status = TQ_ERR;
     if (tq) {
         if (tq->size == tq->capacity) {
-            tq_resize();
+            status = tq_resize();
+            if (status) {
+                return status;
+            }
         }
         // Add to the bottom of the priority queue
         tq->tasks[tq->size] = T;
-        tq_bubble_up();
+        tq_bubble_up(tq->size);
         // Increment the size after bubbling up
         tq->size++;
+        status = TQ_OK;
     }
+    return status;
 }
 
 static Task tq_pop() {
@@ -161,7 +199,7 @@ static Task tq_pop() {
         t = tq->tasks[0];
         tasks[0] = tq->tasks[--tq->size];
         // Decrement the size before bubbling down
-        tq_bubble_down();
+        tq_bubble_down(0);
     }
     return t;
 }
@@ -190,7 +228,9 @@ static void timer_thread(void * arg) {
                 break;
             }
         }
-        tq_set_timer();
+        if (tq_set_timer()) {
+            perror("timer_thread() - Failed to set timer");
+        }
         tq_fair_mutex_unlock();
     }
 }
@@ -200,25 +240,26 @@ void cbscheduler_init() {
 
     tq = (TaskPQ* )malloc(sizeof(TaskPQ));
     if (!tq) {
-        perror("Failed to allocate memory for TaskPQ");
-        exit(-1);
+        perror("cbscheduler_init() - Failed to allocate memory for TaskPQ");
+        exit(TQ_ERR);
     }
     tq->tasks = (Task*)malloc(TQ_INIT_CAPACITY * sizeof(Task));
     if (!(tq->tasks)) {
-        perror("Failed to allocate memory for TaskPQ tasks\n");
-        exit(-1);
+        perror("cbscheduler_init() - Failed to allocate memory for TaskPQ tasks");
+        exit(TQ_ERR);
     }
     tq->size = 0;
     tq->capacity = TQ_INIT_CAPACITY;
     tq->tq_timerid = 0;
+    tq->tq_timer_at = 0;
     if (pthread_mutex_init(&tq->tq_mutex, NULL)) {
-        perror("Failed to initialize tq mutex");
-        exit(-1);
+        perror("cbscheduler_init() - Failed to initialize tq mutex");
+        exit(TQ_ERR);
     }
 
     if (pthread_cond_init(&tq->tq_cond, NULL)) {
-        perror("Failed to initialize tq conditional variable");
-        exit(-1);
+        perror("cbscheduler_init() - Failed to initialize tq conditional variable");
+        exit(TQ_ERR);
     }
     tq->tq_mutex_head = 0;
     tq->tq_mutex_tail = 0;
@@ -228,9 +269,9 @@ void cbscheduler_init() {
     sev.sigev_notify_function = timer_thread;
     sev.sigev_value.sival_ptr = NULL;
 
-    if (timer_create(CLOCK_REALTIME, &sev, &tq->tq_timerid) == -1) {
-        perror("Failed to create timer");
-        exit(-1);
+    if (timer_create(CLOCK_REALTIME, &sev, &tq->tq_timerid) == TQ_ERR) {
+        perror("cbscheduler_init() - Failed to create timer");
+        exit(TQ_ERR);
     }
 
     tq_shutdown = 0;
@@ -245,14 +286,14 @@ void cbscheduler_shutdown() {
         tq_fair_mutex_unlock();
         if (tq->tq_timerid) {
             if (timer_delete(&tq->tq_timerid)) {
-                perror("Failed to disarm tq timer !");
+                perror("cbscheduler_shutdown() - Failed to disarm tq timer !");
             }
         }
         if (pthread_mutex_destroy(&tq->tq_mutex)) {
-            perror("Failed to destroy tq mutex !");
+            perror("cbscheduler_shutdown() - Failed to destroy tq mutex");
         }
         if (pthread_cond_destroy(&tq->tq_cond)) {
-            perror("Failed to destroy tq conditional variable !");
+            perror("cbscheduler_shutdown() - Failed to destroy tq conditional variable");
         }
         if (tq->tasks) {
             free(tq->tasks);
@@ -262,14 +303,25 @@ void cbscheduler_shutdown() {
     }
 }
 
-void schedule(void (*callback)(), uint64_t delay_us)  {
+int schedule(void (*callback)(), uint64_t delay_us)  {
+    int status = TQ_ERR;
     if (callback && delay_us) {
         Task T = {callback, get_current_time_in_us() + delay_us};
         if (tq && !tq_shutdown) {
             tq_fair_mutex_lock();
-            tq_push(T);
-            tq_set_timer();
+
+            if (tq_push(T)) {
+                perror("schedule() - Failed to push task");
+            } else {
+                if (tq_set_timer()) {
+                    perror("schedule() - Failed to set timer after pushing task");
+                } else {
+                    status = TQ_OK;
+                }
+            }
+
             tq_fair_mutex_unlock();
         }
     }
+    return status;
 }
--- callbackScheduler/cbscheduler.h
+++ callbackScheduler/cbscheduler.h
@@ -1,11 +1,21 @@
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
 
+#include <stdint.h>
+
 #define TQ_INIT_CAPACITY 10
 #define TQ_RESIZE_FACTOR 2
 
+// Function prototypes
+
+// Initializes the callback scheduler
 void cbscheduler_init();
-void schedule(void (*callback)(), uint64_t delay_us);
+
+// Schedules a callback to be executed after a delay in microseconds
+// Returns 0 on success, -1 on failure
+int schedule(void (*callback)(), uint64_t delay_us);
+
+// Shuts down the callback scheduler
 void cbscheduler_shutdown();
 
 #endif
